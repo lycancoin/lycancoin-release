c872b50b59233224d94e8370896ad33a34d51a80
Werewolf <lycancoin@lyccoin.org>
2021-11-17 21:18:00 -0800

    enforce version restrictions

--- src/version.h
+++ src/version.h
@@ -15,14 +15,14 @@ static const int PROTOCOL_VERSION = 60004;
 static const int INIT_PROTO_VERSION = 60000;
 
 // In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 60001;
+static const int GETHEADERS_VERSION = 60004;
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 60001;
+static const int CADDR_TIME_VERSION = 60004;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
